705531981539dc04ccb88f00b4b58c86b38726f3
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-03-19 13:49:55 -0700

    Neural Network class without any backpropogation

--- nn.py
+++ nn.py
@@ -13,12 +13,24 @@ y = np.array(([75], [82], [93]), dtype=float)
 x = x/np.amax(x, axis=0)
 y = y/ 100 #max score on the test
 class Neural_Network(object):
-	def _init_(self):
-	#define hyperparameters= parameters that do not change
-	self.inputLayerSize = 2
-	self.outputLayerSize = 1
-	self.hiddenLayerSize = 3
-	
-	def forward(self, x):
-	#propogate inputs through the network
-	
+    def __init__(self):        
+        #Define Hyperparameters
+        self.inputLayerSize = 2
+        self.outputLayerSize = 1
+        self.hiddenLayerSize = 3
+        
+        #Weights (parameters)
+        self.W1 = np.random.randn(self.inputLayerSize, self.hiddenLayerSize)
+        self.W2 = np.random.randn(self.hiddenLayerSize, self.outputLayerSize)
+        
+    def forward(self, X):
+        #Propagate inputs though network
+        self.z2 = np.dot(X, self.W1)
+        self.a2 = self.sigmoid(self.z2)
+        self.z3 = np.dot(self.a2, self.W2)
+        yHat = self.sigmoid(self.z3) 
+        return yHat
+        
+    def sigmoid(self, z):
+        #Apply sigmoid activation function to scalar, vector, or matrix
+        return 1/(1+np.exp(-z))
