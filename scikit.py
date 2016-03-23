e4286dc49c3853ee4c71f0e3a09c57f8ceb43f89
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-03-23 11:39:41 -0700

    Checking to make sure I have the necessary libraries to do some machine learning

---
+++ scikit.py
@@ -0,0 +1,26 @@
+'''
+Working with machine learning algorithms in python
+'''
+
+#all of the following packages are already installed with anaconda with the 
+#exception being seaborn
+from __future__ import print_function
+
+import IPython
+print('IPython:', IPython.__version__)
+
+import numpy
+print('numpy:', numpy.__version__)
+
+import scipy
+print('scipy:', scipy.__version__)
+
+import matplotlib
+print('matplotlib:', matplotlib.__version__)
+
+import sklearn
+print('scikit-learn:', sklearn.__version__)
+
+#conda install seaborn
+import seaborn
+print('seaborn', seaborn.__version__)
